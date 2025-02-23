6ede0fd471f850fe45d0aaccd74e63acbdeb3d5a
Alvaro5-fm <alvarofm1507@gmail.com>
2025-02-23 13:57:39 -0500

    Subiendo primer commit ec2

---
+++ src/main/java/pe/edu/idat/ec2_gateway/Ec2GatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec2_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2GatewayApplication.class, args);
+	}
+
+}
